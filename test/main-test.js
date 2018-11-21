948a8d660ceabf3c5b154ecaf7f89f5f3e39a771
Zhen She <neal.seah@gmail.com>
2018-11-21 22:32:56 +0800

    test: translate test case

--- test/main-test.js
+++ test/main-test.js
@@ -19,13 +19,13 @@ describe('pos', () => {
 
     printReceipt(tags);
 
-    const expectText = `***<没钱赚商店>收据***
-名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
-名称：荔枝，数量：2.5斤，单价：15.00(元)，小计：37.50(元)
-名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
+    const expectText = `***<store earning no money>Receipt ***
+Name: Sprite, Quantity: 5 bottles, Unit price: 3.00 (yuan), Subtotal: 12.00 (yuan)
+Name: Litchi, Quantity: 2.5 kg, Unit price: 15.00 (yuan), Subtotal: 37.50 (yuan)
+Name: Noodles, Quantity: 3 bags, Unit price: 4.50 (yuan), Subtotal: 9.00 (yuan)
 ----------------------
-总计：58.50(元)
-节省：7.50(元)
+Total: 58.50 (yuan)
+Saving: 7.50 (yuan)
 **********************`;
 
     expect(console.log).toHaveBeenCalledWith(expectText);
